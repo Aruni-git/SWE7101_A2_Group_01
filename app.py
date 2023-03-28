81ae0b85dfe3f2860757a311027512c48f94fdc7
aruni-git <aruni.gunapala@gmail.com>
2023-03-28 18:11:17 +0100

    endpoints http file is added , json support addded

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 # import the Flask class from the flask library
-from flask import Flask
+from flask import Flask,jsonify
 # create an instance of the Flask class and assign to app
 # __name__ refers to the default path of the package
 app = Flask(__name__)
 # decorator @ is used to determine path and trigger proceeding function
 @app.get("/")
 def hello_world():
- return "<p>Hello, World!</p>"
+ return "<p>Hello, Welcome to our student management system!</p>"
  
+@app.get("/get-json")
+def get_json():
+ response = [{"forename":"Aruni"},{"surname":"Gunapala"}]
+ return jsonify (response)
