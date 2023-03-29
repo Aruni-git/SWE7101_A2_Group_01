3d20e4707cf6ba4792fd1e42049c905aee71f2db
Yusluv27 <afolamiyusuf@gmail.com>
2023-03-29 16:27:10 +0100

    Updated database tables and reflected on Dbeaver

--- app.py
+++ app.py
@@ -6,13 +6,31 @@ from flask_sqlalchemy import SQLAlchemy
  
 app = Flask(__name__)
  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
  
 # Instantiate db obj
-db = SQLAlchemy(app)
- 
+db = SQLAlchemy()
+
+db.init_app(app)
+
+# Import each table from model.py to app.py so that Flask can run
+from models.model import Attendance
+from models.model import Student
+from models.model import Tutor
+from models.model import Course
+from models.model import Module
+from models.model import Timetable_Event
+
+
+
+    
+with app.app_context():
+    db.create_all()
+
+
  
 @app.get("/home")
 def test_route():
