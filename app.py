bc3761a6ce16f92896ceaea85878547a0d12a795
aruni-git <aruni.gunapala@gmail.com>
2023-04-04 20:06:57 +0100

    Added tutot.py

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
-# import the Flask class from the flask library
+#Author name: Aruni
+#Purpose: Create app.py
+#Date: March 14, 2023
+
+
 from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
+# Marshmallow is an object serialization/deserialization library
+from marshmallow import Schema
+import os
+#import jsonpickle
 
-# create an instance of the Flask class and assign to app
-# __name__ refers to the default path of the package
 app = Flask(__name__)
 
-# decorator @ is used to determine path and trigger proceeding function
-@app.get("/")
-def hello_world():
- return "<p>Hello, Welcome to our student management system!</p>"
- 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///student.db'
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# the name of the database; add path if necessary
+db_name = 'student.db'
 
- 
-# Instantiate db obj
+# these two lines added to mac os support
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, db_name)
+
+#support for windows
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# this variable, db, will be used for all SQLAlchemy commands
 db = SQLAlchemy()
-
 db.init_app(app)
 
+
 # Import each table from model.py to app.py so that Flask can run
+
 from models.model import Attendance
 from models.model import Student
 from models.model import Tutor
@@ -32,11 +41,24 @@ from models.model import Timetable_Event
 with app.app_context():
     db.create_all()
 
-# @app.get("/home")
-# def test_route():
-#     return "Test the app"
- 
-@app.get("/get-json")
-def get_json():
- response = [{"forename":"Aruni"},{"surname":"Gunapala"}]
- return jsonify (response)
+""""@app.route('/')
+def home_route():
+    return "Home"""""
+
+#Create tutor schema
+class TutorSchema(Schema):
+    class Meta:
+        fields = ('tutor_id','tutor_forename','tutor_surname','tutor_email','tutor_category','module_id')
+        
+# Init schema
+tutor_schema = TutorSchema(many =True)
+
+@app.route('/', methods = ['GET'])
+def tefetchLessonsByTutorst():
+    
+    lessons = Tutor.query.all()
+    results = tutor_schema.dump(lessons)
+    
+    return jsonify( results)
+   # return  jsonpickle.encode(tutor,unpicklable=False)
+
