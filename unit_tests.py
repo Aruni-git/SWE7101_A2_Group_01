eaa3891bc5d7d0e40c9c3007c53e3a34e376823a
Aruni Gunapala <55123364+Aruni-git@users.noreply.github.com>
2023-04-23 17:10:48 +0100

    unit_tests.py file added

--- unit_tests.py
+++ unit_tests.py
@@ -2,4 +2,6 @@
 #Purpose: To write unit tests
 #Date; April 23, 2023
 
-import pip._vendor.requests 
+import pip._vendor.requests
+
+def test_get_locations_for_us_90210_check_status_code_equals_200():
